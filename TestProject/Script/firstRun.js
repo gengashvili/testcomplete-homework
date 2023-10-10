83f65f14831dafd86fecaf988b9190403214e9e8
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-10-10 16:42:55 +0400

    finish testcomplete basics homework

---
+++ TestProject/Script/firstRun.js
@@ -0,0 +1,14 @@
+﻿var { openOrder, closeOrder } = require("orderFunctions");
+
+  
+function helloWorld() {
+    Log.Message("hello world!");
+}
+  
+
+function main() {
+  helloWorld();
+  openOrder();
+  Test1();
+  closeOrder();
+}
---
+++ TestProject/Script/orderFunctions.js
@@ -0,0 +1,27 @@
+﻿function openOrder() {
+  TestedApps.Orders.Run();
+}
+
+function closeOrder() {
+  TestedApps.Orders.Close();
+}
+
+function addOrderWithKeywordTest()
+{
+  //Moves the mouse cursor to the menu item specified and then simulates a single click.
+  Aliases.Orders.MainForm.MainMenu.Click("Orders|New order...");
+  //Clicks the 'Customer' object.
+  Aliases.Orders.OrderForm.Group.Customer.Click(17, 3);
+  //Enters the text 'name' in the 'Customer' text editor.
+  Aliases.Orders.OrderForm.Group.Customer.SetText("name");
+  //Clicks the 'ButtonOK' button.
+  Aliases.Orders.OrderForm.ButtonOK.ClickButton();
+}
+
+
+
+module.exports = {
+  openOrder,
+  addOrderWithKeywordTest,
+  closeOrder
+};
