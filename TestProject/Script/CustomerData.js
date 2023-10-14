df9a9a5c2adebbca90916d2a3fac908347ee9784
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-10-14 05:50:41 +0400

    finish patterns homework

---
+++ TestProject/Script/CustomerData.js
@@ -0,0 +1,13 @@
+﻿
+const customer = {
+  name: Project.Variables.customerName,
+  name1: Project.Variables.customerName1,
+  name2: Project.Variables.customerName2,
+  pricePerUnit: Project.Variables.pricePerUnit,
+  discount: Project.Variables.discount,
+  total: Project.Variables.total,
+  invalidDate: Project.Variables.invalidDate
+  
+}
+
+module.exports = customer;
---
+++ TestProject/Script/HomePage.js
@@ -0,0 +1,21 @@
+﻿
+class HomePage {
+  
+  constructor() {
+    // ამ სელექტორს კონსტრუქტორში რომ ვაცხადებ გაშვებისას ვერ პოულობს რატომღაც და ამიტომ დაბლა მეთოდში პირდაპირ ვაკლიკებ
+    //this.mainMenu = Aliases.Orders.MainForm.MainMenu;
+    this.toolBar = Aliases.Orders.wndWindowsForms10Window8app0141b42ar8ad1.ToolBar;
+    this.ordersView = Aliases.Orders.MainForm.OrdersView;
+  }
+  
+  clickAddOrderFromList() {
+    Aliases.Orders.MainForm.MainMenu.Click("Orders|New order...");
+  }
+  
+  clickAddOrderFromBtn() {
+    this.toolBar.clickItem(4);
+  }
+  
+}
+
+module.exports = HomePage;
